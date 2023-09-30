c1e4727788039e35fe49875f3604575ed8d3d1d0
Austinraj <austinraja2509@gmail.com>
2023-09-30 19:35:48 +0530

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (event)=>{
+    const xpos = event.offsetX;
+    const ypos = event.offsetY;
+    const spanel = document.createElement("span");
+    spanel.style.left =xpos + "px";
+    spanel.style.top =ypos + "px";
+    const size = Math.random()*100;
+    spanel.style.width = size + "px";
+    spanel.style.height = size + "px";
+    bodyEl.appendChild(spanel);
+    setTimeout(()=>{
+        spanel.remove();
+    }, 3000);
+  
+});
